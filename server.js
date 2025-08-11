d01cf651bbdeb434843b559513862ec68b13dda3
Lakshya SIngh <iD024>
2025-08-11 22:15:35 +0530

    Added sqlite database

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+let sql;
 
-// fake user database
-// gonna change after setting up
-let users = [];
+// connect to DB
+const db = new sqlite3.Database(
+  "./database/accounts.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log("Connected");
+  }
+);
+
+// this creates a table if it doesn't exist
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      email TEXT UNIQUE,
+      password TEXT NOT NULL
+    )
+  `);
+});
 
 // read the file in disk which is then sent to server to browser for it to load
 function serveFile(res, filePath, contentType) {
@@ -19,20 +41,27 @@ function serveFile(res, filePath, contentType) {
   });
 }
 
-// Here req is the request varable we sent and res is response we get
+/*
+   req – Contains details about the incoming request (URL, method, etc.).
+   res – Used to send a response back to the browser.
+*/
 const server = http.createServer((req, res) => {
   console.log(`Received request`);
   if (req.url === "/auth" && req.method === "GET") {
+    // here /auth is the GET Request
     serveFile(res, path.join(__dirname, "Auth Page", "auth.html"), "text/html");
   } else if (req.url === "/" && req.method === "GET") {
+    //here when we are in / root it tells browser to call index.html
     serveFile(
       res,
       path.join(__dirname, "Landing Page", "index.html"),
       "text/html"
     );
   } else if (req.url.endsWith(".css")) {
+    //if the html asks css it sends css
     serveFile(res, path.join(__dirname, req.url), "text/css");
   } else if (req.url.endsWith(".js")) {
+    //if the html asks js it sends css
     serveFile(res, path.join(__dirname, req.url), "application/javascript");
   } else if (
     req.url.endsWith(".png") ||
@@ -58,6 +87,7 @@ const server = http.createServer((req, res) => {
       mimeTypes[ext] || "application/octet-stream"
     );
   } else {
+    // if nothing is match sends 404
     res.writeHead(404);
     res.end("Not found");
   }
