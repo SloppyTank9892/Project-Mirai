d7d3dec31ddd0eae52e39a085aed64263a21dc4e
SloppyTank9892 <krishnag4746@gmail.com>
2025-08-18 19:50:20 +0530

    Did Nothing

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
+const signInBtn = document.querySelector(".sign-in form button:not(.icon)");
 
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
@@ -8,4 +9,12 @@ registerBtn.addEventListener("click", () => {
 
 loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
+});
+
+signInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.body.classList.add("fade-out");
+  setTimeout(() => {
+    window.location.href = "auth1.html";
+  }, 1000);
 });
